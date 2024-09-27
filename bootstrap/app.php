e965b362992a1a62b404a0a253a6d16ca9744d6d
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2024-09-27 09:38:09 +0100

    Fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+ 
         $middleware->alias([
             'is.admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
